08f8b15d659e90ab174f0a9ae55d9c4118c145d5
Rich Lander <rlander@microsoft.com>
2020-10-16 15:38:52 -0700

    Add binding for HT16K33 chip

---
+++ src/devices/Ht16k33/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ht16k33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
